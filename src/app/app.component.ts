6ed100994219164ea2b1f8f73fde29223f16cab3
KateM3d <ekaterina8medvedeva@gmail.com>
2022-08-25 15:59:03 -0400

    add functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'game-project';
+
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    firedNumber % 2 === 0
+      ? this.evenNumbers.push(firedNumber)
+      : this.oddNumbers.push(firedNumber);
+  }
 }
